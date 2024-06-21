e4d53b4de340b585c44bd559fff0662e13c02404
goosebomb <shj2012@naver.com>
2024-06-21 23:46:31 +0900

    fix: delete forward declaration

--- navigation/moveToGoal.h
+++ navigation/moveToGoal.h
@@ -5,48 +5,41 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
-bool moveToGoal(double xGoal, double yGoal, double zAngle, double wAngle);
+bool moveToGoal(double xGoal, double yGoal, double zAngle, double wAngle)
+{
+    actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base", true);
 
-bool moveToGoal(double xGoal, double yGoal, double zAngle, double wAngle){
+    while (!ac.waitForServer(ros::Duration(5.0)))
+        ROS_INFO("Waiting for the move_base action server to come up");
 
-   //define a client for to send goal requests to the move_base server through a SimpleActionClient
-   actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base", true);
+    move_base_msgs::MoveBaseGoal goal;
 
-   //wait for the action server to come up
-   while(!ac.waitForServer(ros::Duration(5.0))){
-      ROS_INFO("Waiting for the move_base action server to come up");
-   }
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.header.stamp = ros::Time::now();
 
-   move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.pose.position.x = xGoal;
+    goal.target_pose.pose.position.y = yGoal;
+    goal.target_pose.pose.position.z = 0.0;
+    goal.target_pose.pose.orientation.x = 0.0;
+    goal.target_pose.pose.orientation.y = 0.0;
+    goal.target_pose.pose.orientation.z = zAngle;
+    goal.target_pose.pose.orientation.w = wAngle;
 
-   //set up the frame parameters
-   goal.target_pose.header.frame_id = "map";
-   goal.target_pose.header.stamp = ros::Time::now();
+    ROS_INFO("Sending goal location ...");
+    ac.sendGoal(goal);
 
-   /* moving towards the goal*/
+    ac.waitForResult();
 
-   goal.target_pose.pose.position.x =  xGoal;
-   goal.target_pose.pose.position.y =  yGoal;
-   goal.target_pose.pose.position.z =  0.0;
-   goal.target_pose.pose.orientation.x = 0.0;
-   goal.target_pose.pose.orientation.y = 0.0;
-   goal.target_pose.pose.orientation.z = zAngle;
-   goal.target_pose.pose.orientation.w = wAngle;
-
-   ROS_INFO("Sending goal location ...");
-   ac.sendGoal(goal);
-
-   ac.waitForResult();
-
-   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-      ROS_INFO("You have reached the destination");
-      return true;
-   }
-   else{
-      ROS_INFO("The robot failed to reach the destination");
-      return false;
-   }
-
+    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+        ROS_INFO("You have reached the destination");
+        return true;
+    }
+    else
+    {
+        ROS_INFO("The robot failed to reach the destination");
+        return false;
+    }
 }
 
 #endif // MOVETOGOAL_H
